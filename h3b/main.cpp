149392260676e61ca3d7303b267d208ee56ed24b
Taapitoope <taapitoope@users.noreply.github.com>
2025-11-14 15:48:48 +0200

    h3b valmis?

---
+++ h3b/main.cpp
@@ -0,0 +1,16 @@
+#include "animal.h"
+#include "dog.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Animal objElain1;
+    Dog objElain2;
+    objElain1.callOut();
+    objElain2.callOut();
+
+    return 0;
+}
